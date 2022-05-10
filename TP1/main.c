bff0d52ebc0057cef964eca469042ae133eb1356
TomiZubi <tzubizarreta@frba.utn.edu.ar>
2022-05-09 23:44:32 -0300

    TP1

---
+++ TP1/main.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+#define BLOCK 100
+
+char * eliminar ( char * string, int inicio, int fin ) {
+  int i = 0, k = 0;
+  int len = strlen(string);
+  char * aux = malloc(len+1);
+  while ( string[i] != 0 ){
+    if ( i < inicio || i > fin ){
+    aux[k++] = string[i];
+    //printf("\nACA\n");
+    }
+  }
+  return aux;
+}
+
+void evaluar( char * string ) {
+  int l = strlen(string)+1;
+  for ( int i = 0 ; i < l ; i++ ){
+    char c = string[i];
+    if ( isupper(c) )
+      c = tolower(c);
+    else if ( islower(c) )
+      c = toupper(c);
+    /*else if ( c == ','){
+        int k = i;
+        while ('0' <= c <= '9'){
+            i++;
+            string [i] = c;
+        }
+        i = k;
+        string = eliminar (string, k, i);
+    }*/
+    string[i] = c;
+  }
+  return;
+}
+
+
+/*void evaluar( char * string ){
+  char c;
+  int i=0;
+  while (c = getchar()!='\n'){
+    char c = string[i];
+    if ( isupper(c) )
+      c = tolower(c);
+    else if ( islower(c) )
+      c = toupper(c);
+    string[i] = c;
+    i++;
+  }
+}*/
+
+int main() {
+    printf ("Por favor ingrese una palabra: ");
+    char string[BLOCK];
+    scanf ("%s", string);
+    //fgets(string,BLOCK,stdin);
+    gets(string);
+    evaluar(string);
+    printf ("Devuelve %s", string);
+    return 0;
+}
