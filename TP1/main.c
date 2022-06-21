f26aa7767fc803bb3670b2ac8c016f5d30f1402c
TomiZubi <103131505+TomiZubi@users.noreply.github.com>
2022-06-21 09:30:58 -0300

    Update main.c

--- TP1/main.c
+++ TP1/main.c
@@ -2,28 +2,22 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-
-char* evaluar (char string[]) {
-    for (int i = 0; string [i] != '\0'; i++){
-        if (isupper (string[i]))
-            string [i] = tolower (string[i]);
-        else if (islower (string[i]))
-            string [i] = toupper (string[i]);
-        else if (isdigit (string[i]))
-            string [i] = NULL;
-    }
-    return string;
-}
 
 int main()
 {
-    char string [100];
-    char* c;
-    printf ("Por favor ingrese una palabra: ");
-    scanf ("%s", string);
-    c = evaluar (string);
-    printf ("Devuelve %s", c);
+    int p;
+    printf ("Ingrese :");
+    scanf ("%d", p);
+    while ((p=getchar())!= EOF){
+        if (islower (p))
+            putchar (toupper (p));
+        if (isupper (p))
+            putchar (tolower (p));
+        if (ispunct (p))
+            putchar (p) ;
+        if (isspace (p))
+            putchar (p) ;
+    }
+    printf ("Devuelve %d", p);
     return 0;
 }
-
